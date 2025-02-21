b82c0dfeb6857e190b6893731015d9b8eb6b42c0
gane0812 <152599116+gane0812@users.noreply.github.com>
2025-02-21 16:42:27 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,12 @@ resource "azurerm_storage_account" "storage_account" {
   account_replication_type = "LRS"
 }
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rg1" {
   name = "mentor-rg"
+location = "east US"
+}
+
+resource "azurerm_resource_group" "rg2" {
+  name = "mentor-rg-2"
 location = "east US"
 }
