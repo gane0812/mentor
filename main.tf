9c80d50f0bdb6c18c41ab64f2159e1cf017237d6
Ganesh <Ganeshsaravanan@gmail.com>
2025-03-04 13:17:24 +0000

    One Rg called mentor

--- main.tf
+++ main.tf
@@ -35,8 +35,3 @@ resource "azurerm_resource_group" "rg" {
   name = "mentor-rg"
 location = "east US"
 }
-
-resource "azurerm_resource_group" "rg2" {
-  name = "mentor-rg-2"
-location = "east US"
-}
